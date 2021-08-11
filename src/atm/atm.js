5db566c995cc671e0521d6d8d9482ae47c3b0627
Rubens Felix Malagute <rubens.malagute@omega7systems.com>
2021-08-11 20:14:54 -0300

    refactor: refactoring class

--- src/atm/atm.js
+++ src/atm/atm.js
@@ -1,29 +1,59 @@
-function solutions (bills, ammounts, variation, amount, position) {
-  const list = []
-  const value = _compute(bills, variation)
-  if (value < amount) {
-    for (let i = position; i < bills.length; i++) {
-      if (ammounts[i] > variation[i]) {
-        const newvariation = [...variation]
-        newvariation[i]++
-        const newList = solutions(bills, ammounts, newvariation, amount, i)
-        if (newList != null) {
-          list.push(...newList)
+module.exports = class ATM {
+
+  constructor ({ bills, billAmmounts }) {
+    this.bills = bills.sort((a, b) => b - a)
+    this.billAmmounts = billAmmounts.sort((a, b) => b - a)
+  }
+
+  solutions (variation, amount, position) {
+    const list = []
+    const value = this._compute(this.bills, variation)
+    if (value < amount) {
+      for (let i = position; i < this.bills.length; i++) {
+        if (this.billAmmounts[i] > variation[i]) {
+          const newVariation = [...variation]
+          newVariation[i]++
+          const newList = this.solutions(newVariation, amount, i)
+          if (newList != null) {
+            list.push(...newList)
+          }
         }
       }
+    } else if (value === amount) {
+      list.push([...variation])
     }
-  } else if (value === amount) {
-    list.push([...variation])
+    return list
   }
-  return list
-}
 
-function _compute (bills, variation) {
-  let ret = 0
-  for (let i = 0; i < variation.length; i++) {
-    ret += bills[i] * variation[i]
+  _setBills (bills) {
+    this.bills = bills
   }
-  return ret
-}
 
-module.exports = solutions
+  _setBillAmmounts (billAmmounts) {
+    this.billAmmounts = billAmmounts
+  }
+  
+  getConfigurations (ammount) {
+    const originalBills = [...this.bills]
+    const originalBillAmmounts = [...this.billAmmounts]
+    const initialVariation = new Array(this.bills.length).fill(0)
+    const allSolutions = this.solutions(initialVariation, ammount, 0)
+    const configurations = {
+      moreHigherBills: allSolutions[0],
+      moreLowerBills: allSolutions[allSolutions.length - 1]
+    }
+
+    this._setBills(originalBills)
+    this._setBillAmmounts(originalBillAmmounts)
+
+    return configurations
+  }
+
+  _compute (bills, variation) {
+    let ret = 0
+    for (let i = 0; i < variation.length; i++) {
+      ret += bills[i] * variation[i]
+    }
+    return ret
+  }
+}
--- src/atm/atm.test.js
+++ src/atm/atm.test.js
@@ -3,19 +3,35 @@ const ATM = require('./atm')
 describe('ATM', () => {
   it('should return correct solutions for a given withdrawl amount', () => {
     const bills = [100, 50, 20, 10]
-    const billAmounts = [10, 10, 10, 10]
+    const billAmmounts = [10, 10, 10, 10]
+    const atm = new ATM({ bills, billAmmounts })
     const initialVariation = new Array(bills.length).fill(0)
-    const withdrawlAmount = 300
-    const result = ATM(bills, billAmounts, initialVariation, withdrawlAmount, 0)
+    const withdrawAmount = 300
+    const result = atm.solutions(initialVariation, withdrawAmount, 0)
     expect(result.some(element => areEquals(element, [2, 2, 0, 0]))).toBeTruthy()
     expect(result.some(element => areEquals(element, [1, 3, 2, 1]))).toBeTruthy()
   })
+  
+  it('should return two configurations: one with more higher bills, another with more lower bills', () => {
+    const bills = [100, 50, 20, 10]
+    const billAmmounts = [10, 10, 10, 10]
+    const atm = new ATM({ bills, billAmmounts })
+    const configurations = atm.getConfigurations(300)
+    const higherBills = summation(configurations.moreHigherBills)
+    const lower = summation(configurations.moreLowerBills)
+    expect(higherBills).toBeLessThan(lower)
+  })
 
-  function areEquals (arr1, arr2) {
+
+  areEquals = (arr1, arr2) => {
     if (arr1.length !== arr2.length) return false
     for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) return false
     }
     return true
+  }
+
+  summation = (bills) => {
+    return bills.reduce((a, b) => a + b, 0)
   }
 })
